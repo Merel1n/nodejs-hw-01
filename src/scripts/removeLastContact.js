8a4c74ebfac31377142353e277e3521c3414d053
Inna Matviienko <ineskamit@gmail.com>
2024-09-29 18:42:44 +0200

    up

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -5,8 +5,6 @@ export const removeLastContact = async () => {
   try {
     const contacts = await readContacts();
     contacts.pop();
-    console.log(contacts);
-
     writeContacts(contacts);
   } catch (error) {
     console.log(error);
