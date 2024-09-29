c1bfb3665234d1f0224be2687faeac02c5f816a6
Inna Matviienko <ineskamit@gmail.com>
2024-09-29 15:10:57 +0200

    settings

---
+++ src/utils/readContacts.js
@@ -0,0 +1,12 @@
+import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
+
+export const readContacts = async () => {
+  try {
+    const date = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(date);
+    return contacts;
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ src/utils/writeContacts.js
@@ -0,0 +1,11 @@
+import { PATH_DB } from '../constants/constants.js';
+import fs from 'node:fs/promises';
+
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, undefined, 2);
+    await fs.writeFile(PATH_DB, data, 'utf-8');
+  } catch (error) {
+    console.log('ups....', error);
+  }
+};
